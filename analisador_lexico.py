83634baed7a9874294cb5176bc16f36536a85282
Shaiane Boesing <shaianeboesingrb@gmail.com>
2024-07-14 01:26:34 -0300

    retornando em array

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -49,6 +49,7 @@ regex_action = {
                 '[a-zA-Z][a-zA-Z0-9]*': "id",
                 '\s+': "whitespace"
                 }
+
 # Enforcing precedence
 def first_match(token: str):
     for regex in regex_list:
@@ -57,36 +58,46 @@ def first_match(token: str):
     else:
         return None
 
-# Para os Ids
-current_token_string = ''
+def lexer(file_name):
+    # Para os Ids
+    current_token_string = ''
 
-with open("arquivo.txt") as fd:
-    file = fd.read()
-    if file[len(file) -1] == '\n':
-        file = file[:-1]
+    with open(file_name) as fd:
+        file = fd.read()
+        if file[len(file) -1] == '\n':
+            file = file[:-1]
 
-    character_counter = 0
-    was_matched = False
-    while character_counter < len(file):
-        ch = file[character_counter]
+        output = []
+        character_counter = 0
+        was_matched = False
+        while character_counter < len(file):
+            ch = file[character_counter]
 
-        current_token_string += ch
-        matched_regex = first_match(current_token_string)
+            current_token_string += ch
+            matched_regex = first_match(current_token_string)
 
-        if matched_regex is None and was_matched:
-            matched_regex = first_match(current_token_string[:-1])
-            print(regex_action[matched_regex])
+            if matched_regex is None and was_matched:
+                matched_regex = first_match(current_token_string[:-1])
+                token = regex_action[matched_regex]
+                if token != "whitespace":
+                    output.append(token)
 
-            current_token_string = ''
-            character_counter -= 1
-            was_matched = False
-        elif matched_regex is not None and not(was_matched):
-            was_matched = True
+                current_token_string = ''
+                character_counter -= 1
+                was_matched = False
+            elif matched_regex is not None and not(was_matched):
+                was_matched = True
 
-        character_counter += 1
+            character_counter += 1
 
-    # Matching the remaining tokens
-    if first_match(current_token_string) != None:
-        print(regex_action[first_match(current_token_string)])
-    else:
-        print("Error")
+        # Matching the remaining tokens
+        if first_match(current_token_string) != None:
+            token = regex_action[first_match(current_token_string)]
+            if token != "whitespace":
+                output.append(token)
+            output.append("$")
+            print(output)
+        else:
+            print("Error")
+
+lexer("arquivo.txt")
