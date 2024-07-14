74fa8d498bb002c3ab344555e7c26288471fe822
Shaiane Boesing <shaianeboesingrb@gmail.com>
2024-07-13 22:06:22 -0300

    ajuste

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,13 +1,12 @@
 import re
 
-regex_list = ['if', '[a-h]+', ' ']
+regex_list = [r'if',
+              r'[a-h]+',
+              r' ']
 
 regex_action = {'if': "if",
                 '[a-h]+': "id",
                 ' ': "whitespace"}
-
-# Para os Ids
-current_token_string = ''
 
 # Enforcing precedence
 def first_match(token: str):
@@ -17,34 +16,36 @@ def first_match(token: str):
     else:
         return None
 
+# Para os Ids
+current_token_string = ''
+
 with open("arquivo.txt") as fd:
     file = fd.read()
-    if file[len(file) - 1] == '\n':
+    if file[len(file) -1] == '\n':
         file = file[:-1]
 
     character_counter = 0
-    tokens = []
+    was_matched = False
     while character_counter < len(file):
         ch = file[character_counter]
-        current_token_string += ch
 
+        current_token_string += ch
         matched_regex = first_match(current_token_string)
-        
-        if matched_regex:
-            next_ch = file[character_counter + 1] if character_counter + 1 < len(file) else None
-            if next_ch is None or not first_match(current_token_string + next_ch):
-                tokens.append(regex_action[matched_regex])
-                current_token_string = ''
-        
+
+        if matched_regex is None and was_matched:
+            matched_regex = first_match(current_token_string[:-1])
+            print(regex_action[matched_regex])
+
+            current_token_string = ''
+            character_counter -= 1
+            was_matched = False
+        elif matched_regex is not None and not(was_matched):
+            was_matched = True
+
         character_counter += 1
 
     # Matching the remaining tokens
-    if current_token_string:
-        matched_regex = first_match(current_token_string)
-        if matched_regex:
-            tokens.append(regex_action[matched_regex])
-        else:
-            tokens.append("Error")
-
-    for token in tokens:
-        print(token)
+    if first_match(current_token_string) != None:
+        print(regex_action[first_match(current_token_string)])
+    else:
+        print("Error")
